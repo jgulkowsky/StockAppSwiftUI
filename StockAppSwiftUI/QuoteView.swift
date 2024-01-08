e145f53184f034fd3258f311d35cb6d90fb9e91b
jgulkowsky <jgulkowsky@gmail.com>
2024-01-08 13:49:51 +0100

    add price labels, progressView and error views to the QuoteView and manage it through viewModel.state / make sure viewModel methods onViewWillAppear and onViewWillDisappear are called from onAppear and onDisappear / uncomment previews section - however here we need some mock to see anything

--- StockAppSwiftUI/QuoteView.swift
+++ StockAppSwiftUI/QuoteView.swift
@@ -6,25 +6,68 @@
 //
 
 import SwiftUI
+import StockAppLogic
 import StockAppLogicSwiftUI
 
 struct QuoteView: View {
     @ObservedObject var viewModel: QuoteViewModel
     
     var body: some View {
-        Text("It's quote screen :)")
+        Group {
+            if viewModel.state == .loading {
+                ProgressView()
+            } else if viewModel.state == .error {
+                VStack {
+                    Text(viewModel.error ?? "")
+                    Button {
+                        viewModel.onErrorRefreshButtonTapped()
+                    } label: {
+                        Text("Refresh")
+                    }
+                }
+                Spacer()
+            } else if viewModel.state == .dataObtained {
+                VStack {
+                    // todo: add chart view
+                    Spacer()
+                    VStack { // todo: or HStack if there's enough space - when traitCollection.horizontalSizeClass == .regular
+                        HStack {
+                            Text("\(viewModel.bidPrice)")
+                            Spacer()
+                        }
+                        
+                        HStack {
+                            Text("\(viewModel.askPrice)")
+                            Spacer()
+                        }
+                        
+                        HStack {
+                            Text("\(viewModel.lastPrice)")
+                            Spacer()
+                        }
+                    }
+                }
+            }
+        }
+        .padding()
+        .onAppear(perform: viewModel.onViewWillAppear)
+        .onDisappear(perform: viewModel.onViewWillDisappear)
     }
 }
 
-// todo: implement this later on
-//struct QuoteView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        QuoteView(viewModel: QuoteViewModel(
-//            coordinator: <#T##Coordinator#>,
-//            quotesProvider: <#T##QuotesProviding#>,
-//            chartDataProvider: <#T##ChartDataProviding#>,
-//            symbol: <#T##String#>,
-//            refreshRate: <#T##Double#>
-//        )
-//    }
-//}
+// todo: add mocks so we can actually see the QuoteView
+struct QuoteView_Previews: PreviewProvider {
+    static let apiFetcher = ApiFetcher()
+    
+    static var previews: some View {
+        QuoteView(
+            viewModel: QuoteViewModel(
+                coordinator: CoordinatorObject(),
+                quotesProvider: QuotesProvider(apiFetcher: apiFetcher),
+                chartDataProvider: ChartDataProvider(apiFetcher: apiFetcher),
+                symbol: "AAPL",
+                refreshRate: 5
+            )
+        )
+    }
+}
