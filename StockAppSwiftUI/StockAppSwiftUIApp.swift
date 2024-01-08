9216f2b92cd00c97df263697b347fa385eec7688
jgulkowsky <jgulkowsky@gmail.com>
2024-01-08 11:31:56 +0100

    initialize the swiftui project

---
+++ StockAppSwiftUI/StockAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockAppSwiftUIApp.swift
+//  StockAppSwiftUI
+//
+//  Created by Jan Gulkowski on 08/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct StockAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
